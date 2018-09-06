4c3a370545057cfba324fa09b4d5a082a8736b77
huihui <761898617@qq.com>
2018-09-06 15:34:01 +0800

    慧慧

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -7,6 +7,7 @@
             <span class="am-icon-code"></span> 列表
         </div>
     </div>
+    @include('layouts._session')
     <div class="tpl-block">
         <div class="am-g">
             <div class="am-u-sm-12 am-u-md-6">
@@ -17,21 +18,7 @@
                     </div>
                 </div>
             </div>
-            @if(Session::has('success'))
-            <div class=" am-u-sm-12" style="padding:0px;margin:0px;">
-                <div class="dashboard-stat green">
-                        <div class="desc" style="text-align: center;line-height:95px;color:white">{{Session::get('success')}} </div>
-                </div>
-            </div>
-            @endif
-
-            @if(Session::has('error'))
-            <div class=" am-u-sm-12" style="padding:0px;margin:0px;">
-                <div class="dashboard-stat red">
-                        <div class="desc" style="text-align: center;line-height:95px;color:white">{{Session::get('error')}} </div>
-                </div>
-            </div>
-            @endif
+            
             <div class="am-u-sm-12 am-u-md-3">
                 <form action="/user" method="get">
                 <div class="am-input-group am-input-group-sm">
