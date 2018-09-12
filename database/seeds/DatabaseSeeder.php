fe43a7a398fb639fdc95a6fc848029c7bfe60e4d
huihui <761898617@qq.com>
2018-09-12 22:58:41 +0800

    lhh

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(CommentTableSeeder::class);
+        $this->call(OrderTableSeeder::class);
+        $this->call(ShoppingTableSeeder::class);
+        $this->call(ShopUserTableSeeder::class);
     }
 }
